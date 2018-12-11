61335253d8f3d5abd5a8a553f1a8ec9ba595c6c8
Yaojin Sun <sunyaojin2011@foxmail.com>
2018-12-10 19:42:01 -0500

    add build/test to makefile

--- Makefile
+++ Makefile
@@ -3,10 +3,16 @@ GOCMD=go
 GOBUILD=$(GOCMD) build
 GOTEST=$(GOCMD) test -v
 QUESTION1BINARY=keygen
+QUESTION2BINARY_LOCK=lock
+QUESTION2BINARY_UNLOCK=unlock
 
 build:
 	$(GOBUILD) -o $(QUESTION1BINARY) -v keygen.go Cipher.go certificate.go ECCipher.go RSACipher.go cmd.go
+	$(GOBUILD) -o $(QUESTION2BINARY_LOCK) -v lock.go locker.go RSACipher.go cmd.go ECCipher.go certificate.go Cipher.go
+	$(GOBUILD) -o $(QUESTION2BINARY_UNLOCK) -v unlock.go unlocker.go RSACipher.go cmd.go ECCipher.go certificate.go Cipher.go
 test:
 	$(GOTEST) keygen_test.go keygen.go Cipher.go certificate.go ECCipher.go RSACipher.go cmd.go readKeyfileAndSig.go
+	chmod u+x testSetup.sh
+	./testSetup.sh
 clean:
 	rm -rf $(QUESTION1BINARY)
--- testSetup.sh
+++ testSetup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 rm -rf testData
 rm -rf testDirectory
 
@@ -5,7 +7,7 @@ mkdir testData
 mkdir testData/lockingParty
 mkdir testDAta/unlockingParty
 
-mkdir -p testDirectory/{A,B}/{{A1,A2},{B1,B2}} 
+mkdir -p testDirectory/{A,B}/{{A1,A2},{B1,B2}}
 find testDirectory -type d -exec touch {}/TESTFILE \;
 
 echo 'create files'
@@ -16,13 +18,17 @@ find testDirectory -type f -name TESTFILE -exec sh -c 'pwgen 14 1  > "$1"' -- {}
 ./keygen -t ec -s ATypicalSubjectName -pub testData/unlockingParty/wECpub -priv testData/unlockingParty/wECpriv
 ./keygen -t rsa -s ATypicalSubjectName -pub testData/unlockingParty/wRSApub -priv testData/unlockingParty/wRSApriv
 
-go build lock.go locker.go RSACipher.go cmd.go ECCipher.go certificate.go Cipher.go
-go build unlock.go unlocker.go RSACipher.go cmd.go ECCipher.go certificate.go Cipher.go
+echo 'Before Encryption, the sha256 sum of the directory is'
+checksumdir testDirectory
 
 ./lock -d testDirectory  -p testData/unlockingParty/wRSApub -r testData/lockingParty/wECpriv -s ANonTypicalSubjectName
 echo 'locked complete'
-find testDirectory -type f -name TESTFILE -exec sh -c 'head "$1"; echo '\n'' -- {} \;
+#find testDirectory -type f -name TESTFILE -exec sh -c 'head "$1"; echo '\n'' -- {} \;
 
 ./unlock -d testDirectory  -p testData/lockingParty/wECpub -r testData/unlockingParty/wRSApriv -s ANonTypicalSubjectName
 echo 'unlock complete'
-find testDirectory -type f -name TESTFILE -exec sh -c 'cat -t "$1"' -- {} \;
+#find testDirectory -type f -name TESTFILE -exec sh -c 'cat -t "$1"' -- {} \;
+
+echo 'After Encryption, the sha256 sum of the directory is'
+checksumdir testDirectory
+
